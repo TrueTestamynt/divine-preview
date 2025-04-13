1efdcc2f672154fb9be0fae94a31f4163b379902
TrueTestamynt <Testamynt@gmail.com>
2025-04-13 12:28:21 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import DivineHomepage from '../components/DivineHomepage'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Divine Home Health</title>
+      </Head>
+      <DivineHomepage />
+    </>
+  )
+}
